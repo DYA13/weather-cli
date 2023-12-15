542a0b60b426305efec41536aa471af093fc6b18
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-12-15 16:05:01 +0100

    modified:   helpers/args.js modified:   package.json modified:   weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
+import { getArgs } from "./helpers/args.js"
 const initCLI = () => {
-  console.log(process.argv)
+  const args = getArgs(process.argv)
+  console.log(args)
+  if (args.help) {
+    // printing help
+  }
+  if (args.s) {
+    // saving town
+  }
+  if (args.t) {
+    // saving token
+  }
+  // printing weather
 }
 initCLI()
