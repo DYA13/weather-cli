5048ee91cb3c11221a4b0ecbf7edc70e2b2f0db1
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-12-20 17:11:46 +0100

    functions printError, printSuccess, printHelp created in services/log.service.js

--- services/log.service.js
+++ services/log.service.js
@@ -1,3 +1,21 @@
+import chalk from "chalk"
 const printError = (error) => {
-  console.log(`Error` + " " + error)
+  console.log(chalk.bgRed(" ERROR ") + " " + `Error` + " " + error)
 }
+
+const printSuccess = (message) => {
+  console.log(chalk.bgGreen(" SUCCESS ") + " " + message)
+}
+
+const printHelp = () => {
+  console.log(
+    `${chalk.bgCyan(" HELP ")} 
+    Without parameters-weather input
+    -s [CITY] - get weather for city
+    -h - get help
+    -t [API_KEY] - save token
+  `
+  )
+}
+
+export { printError, printSuccess, printHelp }
--- weather.js
+++ weather.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js"
+import { printHelp } from "./services/log.service.js"
 const initCLI = () => {
   const args = getArgs(process.argv)
-  console.log(args)
-  if (args.help) {
-    // printing help
+
+  if (args.h) {
+    printHelp()
   }
   if (args.s) {
     // saving town
